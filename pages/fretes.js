c4804645f548d97c17d914295e164c2ab7514c80
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-18 23:08:19 -0300

    *

--- pages/fretes.js
+++ pages/fretes.js
@@ -8,45 +8,168 @@ import Error from 'next/error';
 import Pagination from "react-pagination-library";
 import "react-pagination-library/build/css/index.css"; //for css
 
+import AsyncSelect from 'react-select/lib/Async';
+
 import Layout from '../components/layout'
 import FretesList from '../components/fretesList'
 
-const URL = 'https://fretesbrasil.herokuapp.com/api/filtro/'
-
+const URL = 'http://localhost:5000'
 export default class Fretes extends Component {
   constructor(props) {
     super(props)
     this.state = { 
-      currentPage: (this.props.match.params.id || 1),
+      currentPage: (this.props.id || 1),
       totalPages: 1,
       data: [],
     }
-    this.changeCurrentPage((this.props.match.params.id || 1))
-  } 
+  }
+
+  componentDidMount() {
+    this.changeCurrentPage(this.state.currentPage)
+  }
 
   changeCurrentPage = page => {
-    axios.post(URL, { page }).then(resp => this.setState({
+    const body = { page }
+    axios.post(URL + '/api/filtro/', body).then(resp => this.setState({
       currentPage: page,
       data: resp.data,
       totalPages: Math.ceil(resp.data.maxitens / resp.data.data.length)
     }))
+
+  SelectEstadoOrigem = estado => {
+    console.log(estado)
+  }
 };
 
   render () {
     return (
       <Layout>
-        <h1>Fretes #{this.state.currentPage}</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-        <FretesList data={this.state.data.data}/>
-        <Pagination
-          currentPage={this.props.currentPage}
-          totalPages={this.state.totalPages}
-          changeCurrentPage={this.changeCurrentPage}
-          theme="bottom-border"
-        />
+        <div className="container ">
+        <div className="col-12 mb-12 my-3 mt-4">
+            <h5>Encontre as melhores ofertas de Fretes</h5>
+            <div className="form-row">
+                <div className="form-group-responsive sm-3 mr-2 mt-3">
+                    <select onChange={this.SelectEstadoOrigem} className="form-control" id="selectEstado">
+                        <option>Estado de Origem</option>
+                        <option>São Paulo</option>
+                        <option>Rio de Janeiro</option>
+                    </select>
+                </div>
+                <div className="form-group sm-3 mr-5 mt-3">
+                    <select className="form-control" id="selectCidade">
+                        <option>Cidade de Origem</option>
+                        <option>Araraquara</option>
+                        <option>Araras</option>
+                        <option>Arco-Íris</option>
+                        <option>Arealva</option>
+                        <option>Areias</option>
+                        <option>Areiópolis</option>
+                        <option>Ariranha</option>
+                        <option>Artur Nogueira</option>
+                        <option>Arujá</option>
+                        <option>Aspásia</option>
+                        <option>Assis</option>
+                        <option>Atibaia</option>
+                        <option>Auriflama</option>
+                        <option>Avaí</option>
+                    </select>
+                </div>
+                <div className="form-group sm-3 mr-2 mt-3">
+                    <select className="form-control" id="selectEstado">
+                        <option>Estado de Destino</option>
+                        <option>São Paulo</option>
+                    </select>
+                </div>
+                <div className="form-group sm-3 mr-5 mt-3">
+                    <select className="form-control" id="selectCidade">
+                        <option>Cidade de Destino</option>
+                        <option>Araraquara</option>
+                        <option>Araras</option>
+                        <option>Arco-Íris</option>
+                        <option>Arealva</option>
+                        <option>Areias</option>
+                        <option>Areiópolis</option>
+                        <option>Ariranha</option>
+                        <option>Artur Nogueira</option>
+                        <option>Arujá</option>
+                        <option>Aspásia</option>
+                        <option>Assis</option>
+                        <option>Atibaia</option>
+                        <option>Auriflama</option>
+                        <option>Avaí</option>
+                    </select>
+                </div>
+                <div className="col-sm-2 ml-auto mt-3">
+                    <button type="submit" id="botao" className="btn ">Pesquisar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+      <div className="container-fluid mb-3 ">
+        <form>
+            <div className="form-row">
+                <div className="container-3">
+                    <div className="col">
+                        <ul className="list-group">
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Carreta LS Trucada
+                              <span className="badge  badge-pill">14</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Carreta Toco
+                              <span className="badge  badge-pill">4</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Bitrem
+                              <span className="badge  badge-pill">5</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Bi-Truck
+                              <span className="badge badge-pill">10</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Truck
+                              <span className="badge  badge-pill">88</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Carreta LS Trucada
+                              <span className="badge  badge-pill">14</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Carreta Toco
+                              <span className="badge  badge-pill">4</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Bitrem
+                              <span className="badge badge-pill">5</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Bi-Truck
+                              <span className="badge  badge-pill">10</span></a>
+                            </li>
+                            <li className="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">Truck
+                              <span className="badge badge-pill">88</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="col ml-5">
+                   <FretesList data={this.state.data.data}/>
+                    <Pagination
+                      currentPage={this.state.currentPage}
+                      totalPages={this.state.totalPages}
+                      changeCurrentPage={this.changeCurrentPage}
+                      theme="bottom-border"
+                    />
+                    <Link href='/a' as='/b'><a>b</a></Link>
+                    <Link href={{ pathname: '/fretes', query: { id: '2' } }} as='/fretes/2'><a>post #2</a></Link>
+
+                </div>
+            </div>
+        </form>
+    </div>
       </Layout>
     )
   }
