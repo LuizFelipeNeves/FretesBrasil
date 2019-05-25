d17dd3188da0b9691e6a94cfeca54ebe78ec20e0
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-25 20:38:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,42 @@
-const express = require('express')
-const next = require('next')
+/* eslint-disable no-shadow */
+/* eslint-disable no-nested-ternary */
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const api = require('./API');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
-  
-  server.get('/', (req, res) => {
-    return app.render(req, res, '/', req.query)
-  })
+  const server = express();
+  server.use(cors());
+  server.use(bodyParser.urlencoded({ extended: false }));
+  server.use(bodyParser.json());
+  server.use((error, req, res, next) => {
+    if (error !== null) return res.json({ err: ['invalid json'] });
+    return next();
+  });
 
-  server.get('/contato', (req, res) => {
-    return app.render(req, res, '/contato', req.query)
-  })
+  server.use('/api', api); // api
+
+  server.get('/', (req, res) => app.render(req, res, '/', req.query));
+
+  server.get('/contato', (req, res) => app.render(req, res, '/contato', req.query));
 
   server.get('/fretes/:id', (req, res) => {
-    return app.render(req, res, '/fretes', { id: req.params.id })
-  })
+    return app.render(req, res, '/fretes', { id: req.params.id });
+  });
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+  server.get('*', (req, res) => handle(req, res));
 
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
-
+  server.listen(port, (err) => {
+    if (err) throw err;
+    // eslint-disable-next-line no-console
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
