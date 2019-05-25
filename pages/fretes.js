67ddef64ea1bb4aba919d1ef97f93f8477091d0c
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-25 07:04:10 -0300

    Update fretes.js

--- pages/fretes.js
+++ pages/fretes.js
@@ -3,36 +3,321 @@ import axios from 'axios'
 
 import Link from 'next/link'
 import Error from 'next/error';
-//import fetch from 'isomorphic-unfetch';
 
 import Pagination from "react-pagination-library";
 import "react-pagination-library/build/css/index.css"; //for css
 
+import Select from "react-select"; // https://react-select.com
+
+import { stringify } from "query-string";
+
 import Layout from '../components/layout'
 import FretesList from '../components/fretesList'
 
-const URL = 'https://fretesbrasil.herokuapp.com'
+const BASE = 'https://fretesbrasil.herokuapp.com'
 
-export default class Fretes extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { 
-      currentPage: (this.props.id || 1),
-      totalPages: 1,
-      data: [],
-    }
-    this.changeCurrentPage((this.props.id || 1))
-  } 
-
-  changeCurrentPage = page => {
-    axios.post(URL + '/api/filtro/', { page }).then(resp => this.setState({
-      currentPage: page,
-      data: resp.data,
-      totalPages: Math.ceil(resp.data.maxitens / resp.data.data.length)
-    }))
+const customStyles = {
+  control: base => ({
+    ...base,
+    height: 45,
+    minHeight: 45,
+    width: 190,
+  })
 };
 
+export default class Fretes extends Component {
+  state = {
+    currentPage: (this.props.id || 1),
+    totalPages: 1,
+    data: [],
+
+    // filtros
+
+    veiculo: false,
+    veiculos: [],
+
+    cidadeOrigem: false,
+    cidadeODisable: true,
+    cidadesO: [],
+
+    estadoOrigem: false,
+    estadosO: [],
+
+    cidadeDestino: false,
+    cidadeDDisable: true,
+    cidadesD: [],
+
+    estadoDestino: false,
+    estadosD: [],
+
+    error: null
+  };
+
+  changeCurrentPage = page => {
+      const body = { page }
+      if (this.state.veiculo) body.veiculo = this.state.veiculo;
+      if (this.state.estadoOrigem.value) body.estadoorigem = this.state.estadoOrigem.value;
+      if (this.state.cidadeOrigem.value) body.cidadeorigem = this.state.cidadeOrigem.value;
+      if (this.state.estadoDestino.value) body.estadodestino = this.state.estadoDestino.value;
+      if (this.state.cidadeDestino.value) body.cidadedestino = this.state.cidadeDestino.value;
+
+      console.log(body)
+
+      axios.post(BASE + '/api/filtro/', body).then(resp => this.setState({
+        currentPage: page,
+        data: resp.data,
+        totalPages: Math.ceil(resp.data.maxitens / 20)
+      }));
+  }
+
+  toggleEstadoO = estado => {
+    if (this.state.estadoOrigem !== estado) {
+      this.setState(
+        {
+          estadoOrigem: estado,
+          cidadeOrigem: false,
+          currentPage: 1,
+        },
+        this.EOrigem
+      );
+    }
+  };
+
+  toggleEstadoD = estado => {
+    if (this.state.estadoDestino !== estado) {
+      this.setState(
+        {
+          estadoDestino: estado,
+          cidadeDestino: false,
+          currentPage: 1,
+        },
+        this.EDestino
+      );
+    }
+  };
+
+  toggleCidadeO = cidade => {
+    if (this.state.cidadeOrigem !== cidade)
+      this.setState({ cidadeOrigem: cidade, currentPage: 1 }, this.COrigem);
+  };
+  toggleCidadeD = cidade => {
+    if (this.state.cidadeDestino !== cidade)
+      this.setState({ cidadeDestino: cidade, currentPage: 1 }, this.CDestino);
+  };
+
+  toggleVeiculo = veiculo => {
+    if (this.state.veiculo !== veiculo) {
+      this.setState({ veiculo: veiculo, currentPage: 1 }, this.AttVeiculos);
+    }
+  };
+
+  async AttVeiculos() {
+    await this.fetchEstadosO();
+    if (this.state.estadoOrigem) await this.fetchCidadesO();
+    await this.fetchEstadosD();
+    if (this.state.estadoDestino) await this.fetchCidadesD();
+    await this.fetchVeiculos();
+    await this.changeCurrentPage(1);
+  }
+
+  async EOrigem() {
+    await this.fetchEstadosD();
+    await this.fetchCidadesO();
+    await this.fetchVeiculos();
+    await this.changeCurrentPage(1);
+  }
+  async EDestino() {
+    await this.fetchEstadosO();
+    await this.fetchCidadesD();
+    await this.fetchVeiculos();
+    await this.changeCurrentPage(1);
+  }
+
+  async COrigem() {
+    await this.fetchEstadosD();
+    await this.fetchVeiculos();
+    await this.changeCurrentPage(1);
+  }
+
+  async CDestino() {
+    await this.fetchEstadosO();
+    await this.fetchVeiculos();
+    await this.changeCurrentPage(1);
+  }
+
+  async fetchEstadosO() {
+    let body = {};
+    if (this.state.veiculo) body.veiculo = this.state.veiculo;
+    if (this.state.estadoDestino.value)
+      body.estadodestino = this.state.estadoDestino.value;
+    if (this.state.cidadeDestino.value)
+      body.cidadedestino = this.state.cidadeDestino.value;
+
+    let URL = BASE + "/api/estadoso";
+    if (stringify(body) !== "") URL += `?${stringify(body)}`;
+    //console.log(URL);
+
+    const data = await fetch(URL)
+      .then(response => response.json())
+      .catch(error => this.setState({ error }));
+
+    this.setState({
+      estadosO: data.data.map(estado => ({
+        value: estado.nome,
+        label: estado.nome
+      }))
+    });
+  }
+
+  async fetchEstadosD() {
+    let body = {};
+    if (this.state.veiculo) body.veiculo = this.state.veiculo;
+    if (this.state.estadoOrigem.value) {
+      body.estadoorigem = this.state.estadoOrigem.value;
+
+      if (this.state.cidadeOrigem.value)
+        body.cidadeorigem = this.state.cidadeOrigem.value;
+    }
+
+    let URL = BASE + "/api/estadosd";
+    if (stringify(body) !== "") URL += `?${stringify(body)}`;
+    //console.log(URL);
+
+    const data = await fetch(URL)
+      .then(response => response.json())
+      .catch(error => this.setState({ error }));
+
+    this.setState({
+      estadosD: data.data.map(estado => ({
+        value: estado.nome,
+        label: estado.nome
+      }))
+    });
+  }
+
+  async fetchCidadesO() {
+    let body = {};
+
+    if (this.state.veiculo) body.veiculo = this.state.veiculo;
+    if (this.state.estadoOrigem.value) {
+      if (this.state.estadoDestino.value) {
+        body.estadodestino = this.state.estadoDestino.value;
+        if (this.state.cidadeDestino.value)
+          body.cidadedestino = this.state.cidadeDestino.value;
+      }
+      body.estadoorigem = this.state.estadoOrigem.value;
+      let URL = BASE + "/api/cidadeo";
+      if (stringify(body) !== "") URL += `?${stringify(body)}`;
+      //console.log(URL);
+
+      const data = await fetch(URL)
+        .then(response => response.json())
+        .catch(error => this.setState({ error }));
+
+      this.setState({
+        cidadeODisable: false,
+        cidadeOrigem: false,
+        cidadesO: data.data.map(cidade => ({
+          value: cidade,
+          label: cidade
+        }))
+      });
+    } else
+      this.setState({
+        cidadeODisable: true,
+        cidadeOrigem: false,
+        cidadesO: []
+      });
+  }
+
+  async fetchCidadesD() {
+    let body = {};
+    if (this.state.veiculo) body.veiculo = this.state.veiculo;
+    if (this.state.estadoDestino.value) {
+      if (this.state.estadoOrigem.value) {
+        body.estadoorigem = this.state.estadoOrigem.value;
+        if (this.state.cidadeOrigem.value)
+          body.cidadeorigem = this.state.cidadeOrigem.value;
+      }
+      body.estadodestino = this.state.estadoDestino.value;
+      let URL = BASE + "/api/cidaded";
+      if (stringify(body) !== "") URL += `?${stringify(body)}`;
+      //console.log(URL);
+
+      const data = await fetch(URL)
+        .then(response => response.json())
+        .catch(error => this.setState({ error }));
+
+      this.setState({
+        cidadeDDisable: false,
+        cidadeDestino: false,
+        cidadesD: data.data.map(cidade => ({
+          value: cidade,
+          label: cidade
+        }))
+      });
+    } else
+      this.setState({
+        cidadeDDisable: true,
+        cidadeDestino: false,
+        cidadesD: []
+      });
+  }
+
+  async fetchVeiculos() {
+    let body = {};
+    if (this.state.veiculo) body.veiculo = this.state.veiculo;
+    if (this.state.estadoOrigem.value)
+      body.estadoorigem = this.state.estadoOrigem.value;
+    if (this.state.cidadeOrigem.value)
+      body.cidadeorigem = this.state.cidadeOrigem.value;
+
+    if (this.state.estadoDestino.value)
+      body.estadodestino = this.state.estadoDestino.value;
+
+    if (this.state.cidadeDestino.value)
+      body.cidadedestino = this.state.cidadeDestino.value;
+
+    let URL = BASE + "/api/veiculos";
+    if (stringify(body) !== "") URL += `?${stringify(body)}`;
+    //console.log(URL);
+
+    const data = await fetch(URL)
+      .then(response => response.json())
+      .catch(error => this.setState({ error }));
+
+    this.setState({ veiculos: data.data });
+  }
+
+  componentDidMount() {
+    this.changeCurrentPage((this.props.id || 1))
+    this.fetchEstadosO();
+    this.fetchEstadosD();
+    this.fetchVeiculos();
+  }
+
+  
+
   render () {
+    const {
+      veiculo,
+      veiculos,
+      estadosO,
+      cidadesO,
+      estadosD,
+      cidadesD,
+      estadoOrigem,
+      cidadeOrigem,
+      estadoDestino,
+      cidadeDestino,
+      cidadeODisable,
+      cidadeDDisable,
+    } = this.state;
+    
+    if (!estadosO[0] || !estadosD[0]) {
+      return <div>Carregando...</div>;
+    }
+
     return (
       <Layout>
         <div className="container ">
@@ -40,57 +325,54 @@ export default class Fretes extends Component {
             <h5>Encontre as melhores ofertas de Fretes</h5>
             <div className="form-row">
                 <div className="form-group-responsive sm-3 mr-2 mt-3">
-                    <select className="form-control" id="selectEstado">
-                        <option>Estado de Origem</option>
-                        <option>São Paulo</option>
-                    </select>
+                  <Select
+                    className="basic-single"
+                    placeholder="Estado de Origem"
+                    value={estadoOrigem}
+                    onChange={this.toggleEstadoO}
+                    name="estadoOrigem"
+                    isSearchable={true}
+                    options={[{ label: "Todos", value: false }, ...estadosO]}
+                    styles={customStyles}
+                  />
                 </div>
                 <div className="form-group sm-3 mr-5 mt-3">
-                    <select className="form-control" id="selectCidade" >
-                        <option>Cidade de Origem</option>
-                        <option>Araraquara</option>
-                        <option>Araras</option>
-                        <option>Arco-Íris</option>
-                        <option>Arealva</option>
-                        <option>Areias</option>
-                        <option>Areiópolis</option>
-                        <option>Ariranha</option>
-                        <option>Artur Nogueira</option>
-                        <option>Arujá</option>
-                        <option>Aspásia</option>
-                        <option>Assis</option>
-                        <option>Atibaia</option>
-                        <option>Auriflama</option>
-                        <option>Avaí</option>
-                    </select>
+                    <Select
+                    className="basic-single"
+                    placeholder="Cidade de Origem"
+                    onChange={this.toggleCidadeO}
+                    value={cidadeOrigem}
+                    name="cidadeOrigem"
+                    isDisabled={cidadeODisable}
+                    isSearchable={true}
+                    options={[{ label: "Todos", value: false }, ...cidadesO]}
+                    styles={customStyles}
+                  />
                 </div>
                 <div className="form-group sm-3 mr-2 mt-3">
-                    <select className="form-control" id="selectEstado">
-                        <option>Estado de Destino</option>
-                        <option>São Paulo</option>
-                    </select>
+                    <Select
+                      className="basic-single"
+                      placeholder="Estado de Destino"
+                      onChange={this.toggleEstadoD}
+                      value={estadoDestino}
+                      name="estadoDestino"
+                      isSearchable={true}
+                      options={[{ label: "Todos", value: false }, ...estadosD]}
+                      styles={customStyles}
+                    />
                 </div>
                 <div className="form-group sm-3 mr-5 mt-3">
-                    <select className="form-control" id="selectCidade">
-                        <option>Cidade de Destino</option>
-                        <option>Araraquara</option>
-                        <option>Araras</option>
-                        <option>Arco-Íris</option>
-                        <option>Arealva</option>
-                        <option>Areias</option>
-                        <option>Areiópolis</option>
-                        <option>Ariranha</option>
-                        <option>Artur Nogueira</option>
-                        <option>Arujá</option>
-                        <option>Aspásia</option>
-                        <option>Assis</option>
-                        <option>Atibaia</option>
-                        <option>Auriflama</option>
-                        <option>Avaí</option>
-                    </select>
-                </div>
-                <div className="col-sm-2 ml-auto mt-3">
-                    <button type="submit" id="botao" className="btn ">Pesquisar</button>
+                    <Select
+                    className="basic-single"
+                    placeholder="Cidade de Destino"
+                    onChange={this.toggleCidadeD}
+                    value={cidadeDestino}
+                    name="cidadeDestino"
+                    isDisabled={cidadeDDisable}
+                    isSearchable={true}
+                    options={[{ label: "Todos", value: false }, ...cidadesD]}
+                    styles={customStyles}
+                  />
                 </div>
             </div>
         </div>
@@ -101,46 +383,32 @@ export default class Fretes extends Component {
                 <div className="container-3">
                     <div className="col">
                         <ul className="list-group">
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Carreta LS Trucada
-                              <span className="badge  badge-pill">14</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Carreta Toco
-                              <span className="badge  badge-pill">4</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Bitrem
-                              <span className="badge  badge-pill">5</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Bi-Truck
-                              <span className="badge badge-pill">10</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Truck
-                              <span className="badge  badge-pill">88</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Carreta LS Trucada
-                              <span className="badge  badge-pill">14</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Carreta Toco
-                              <span className="badge  badge-pill">4</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Bitrem
-                              <span className="badge badge-pill">5</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Bi-Truck
-                              <span className="badge  badge-pill">10</span></a>
-                            </li>
-                            <li className="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="#">Truck
-                              <span className="badge badge-pill">88</span></a>
-                            </li>
+                        {veiculo ? (<li style={{ color: "red" }} key={veiculo} className='list-group-item d-flex justify-content-between align-items-center'>
+                            <a onClick={() => this.toggleVeiculo(false)}>{veiculo}</a>{" "}
+                          </li>) : ("")}
+                        
+
+                        <li key="Total" className='list-group-item d-flex justify-content-between align-items-center'>
+                          <a onClick={() => this.toggleVeiculo(false)}>Total</a>{" "}
+                          {/*veiculototal*/}
+                        </li>
+
+                          {veiculos.map(veic => {
+                            if (veiculo !== veic.nome)
+                              return (
+                                <li
+                                  //style={veiculo === veic.nome ? { color: "red" } : {}}
+                                  //className={veiculo === veic.nome ? "active" : ""}
+                                  className='list-group-item d-flex justify-content-between align-items-center'
+                                  key={veic.nome}
+                                >
+                                  <a onClick={() => this.toggleVeiculo(veic.nome)}>
+                                    {veic.nome} - <span className="badge  badge-pill badge-secondary">{veic.total}</span>
+                                  </a>
+                                </li>
+                              );
+                            return null;
+                          })}
                         </ul>
                     </div>
                 </div>
@@ -153,8 +421,15 @@ export default class Fretes extends Component {
                       changeCurrentPage={this.changeCurrentPage}
                       theme="bottom-border"
                     />
-                    <Link href='/a' as='/b'><a>b</a></Link>
-                    <Link href={{ pathname: '/fretes', query: { id: '2' } }} as='/fretes/2'><a>post #2</a></Link>
+
+                    {
+                      /*
+                      <Link href='/a' as='/b'><a>b</a></Link>
+                      <Link href={{ pathname: '/fretes', query: { id: '2' } }} as='/fretes/2'><a>post #2</a></Link>
+
+                      */
+                    }
+                    
 
                 </div>
             </div>
