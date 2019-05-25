d0e34f9c64783f612688373e155a5b2be187952e
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-25 20:38:07 -0300

    Create Frete.js

---
+++ API/models/Frete.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+const FreteSchema = new mongoose.Schema({
+  _id: Number,
+  url: String,
+  cidadeorigem: String,
+  cidadedestino: String,
+  estadoorigem: String,
+  estadodestino: String,
+  km: String,
+  preco: String,
+  peso: String,
+  veiculo: [{ type: String }],
+  site: String,
+}, { _id: false, timestamps: { createdAt: 'created_at' } });
+
+FreteSchema.plugin(AutoIncrement);
+module.exports = mongoose.model('Frete', FreteSchema);
