17027954f310d210aa5a6a074a68fa2822043957
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-27 08:07:06 -0300

    Carousel

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
 import Contact from '../components/contact'
 
 import { Carousel, Image } from 'react-bootstrap';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 export default () => (
   <div>
+  <Head>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/prop-types/15.6.1/prop-types.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/react/16.3.2/umd/react.production.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/react-dom/16.3.2/umd/react-dom.production.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/react-transition-group/2.2.1/react-transition-group.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/react-popper/0.10.4/umd/react-popper.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/reactstrap/6.0.1/reactstrap.min.js"></script>
+  </Head>
   <Layout> 
     
     {/*
@@ -17,21 +29,21 @@ export default () => (
     <Carousel>
         <Carousel.Item>
             <Image src="static/img/img1.jpg" className="img-fluid d-block w-100"/>
-            <Carousel.Caption>
+            <Carousel.Caption className='d-none d-md-block'>
               <h3>Economize tempo e reduza custos de frete</h3>
               <p>Na hora de enviar um frete cada minuto conta. Encontre o veiculo e motorista que precisa em minutos, contrate diretamente e melhore a performance do seu negócio com uma redução de custos de transporte de até 30%.</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <Image src="static/img/img2.jpg" className="img-fluid d-block w-100"/>
-            <Carousel.Caption>
+            <Carousel.Caption className='d-none d-md-block'>
               <h3>Contrate e acompanhe o serviço em tempo real</h3>
               <p>Divulgar seu frete é apenas o começo. A plataforma FRETES BRASIL oferece uma solução completa: da publicação à contratação, gerenciamento e avaliação do serviço prestado, estamos com você em todos os passos.</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <Image src="static/img/img3.jpg" className="img-fluid d-block w-100"/>
-            <Carousel.Caption>{/* d-none d-md-block */}
+            <Carousel.Caption className='d-none d-md-block'>
               <h3>Segurança, confiabilidade e eficiência</h3>
               <p>Confie seu frete aos melhores profissionais. O FRETES BRASIL traz a eficiência e a qualidade de serviço que seu negócio precisa, te conectando com a maior e mais qualificada base de caminhoneiros autônomos do Brasil.</p>
             </Carousel.Caption>
