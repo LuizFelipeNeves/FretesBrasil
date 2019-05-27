c73cece60a91833922503f671ec3952b652a079f
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-26 23:11:01 -0300

    Update Frete.js

--- API/models/Frete.js
+++ API/models/Frete.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 
-const AutoIncrement = require('mongoose-sequence')(mongoose);
-
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
 const FreteSchema = new mongoose.Schema({
-  _id: Number,
-  url: String,
+  _id: {
+    type: Number,
+    unique: true,
+  },
+  url: {
+    type: String,
+    unique: true,
+  },
   cidadeorigem: String,
   cidadedestino: String,
   estadoorigem: String,
@@ -16,8 +22,20 @@ const FreteSchema = new mongoose.Schema({
   preco: String,
   peso: String,
   veiculo: [{ type: String }],
-  site: String,
+  carroceria: [{ type: String }],
+  anunciante: {
+    type: String,
+    required: true,
+  },
+  nivel: {
+    type: Number,
+    default: 0,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
 }, { _id: false, timestamps: { createdAt: 'created_at' } });
 
-FreteSchema.plugin(AutoIncrement, { id: 'fretes', inc_field: '_id' });
+FreteSchema.plugin(AutoIncrement);
 module.exports = mongoose.model('Frete', FreteSchema);
