938508e835dee67ca67d4e2e9b20084c1c3b7e27
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-05-27 08:58:19 -0300

    about

---
+++ components/about.js
@@ -0,0 +1,32 @@
+export default () => {
+    return (
+    <div className="container-fluid bg-dark" id="sobre">
+        <div className="row ">
+            <div className="col-md-12 text-center text-white b-5 mb-lg-20 my-5">
+                <h3 className="text-uppercase mb-4 text-white">Fretes Brasil</h3>
+                <div className="container-fluid md-12 mb-5 mb-lg-0 my-5">
+                    <div className="row">
+                        <div className="col text-center ">
+                            <p className="lead mb-0 text-white">A FRETES BRASIL fundada em 2019 por pessoas com vasta experiência no segmento de transportes, que acreditaram que a qualidade e prestação de serviços de transporte de cargas necessitavam de atualização e compromisso, deste momento surgiu a ideia de criar uma empresa que pudesse ser líder de mercado atendendo a todos seus clientes com transparência, qualidade, eficácia e agilidade.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="row ">
+            <div className="col-md-4 text-center text-white mb-5 mb-lg-20 my-5">
+                <h1 className="display-4"><i className="mt-2 fas fa-truck"></i> </h1>
+                <p className="lead mb-0 text-black">Motoristas autônomos, carreteiros, transportadoras e embarcadores economizem tempo e dinheiro através da utilização de nossas ferramentas de comunicação interativa.</p>
+            </div>
+            <div className="col-md-4 text-center text-white mb-5 mb-lg-20 my-5">
+                <h1 className="display-4"><i className="fas fa-user-friends "></i></h1>
+                <p className="lead mb-0 text-black">Criada por Ciro, Felipe, Flavio, Ícaro, Lucas e Luiz Felipe, a FRETES BRASIL tem foco no desenvolvimento de soluções para o setor de transporte de cargas.</p>
+            </div>
+            <div className="col-md-4 text-center text-white mb-5 mb-lg-20 my-5">
+                <h1 className="display-4"><i className="mt-2 fas fa-shopping-cart "></i></h1>
+                <p className="lead mb-0 text-black">Nossa equipe presta assessoria que ajuda os motoristas a receberem o pagamento do saldo do frete.</p>
+            </div>
+        </div>
+    </div>
+    );
+}
